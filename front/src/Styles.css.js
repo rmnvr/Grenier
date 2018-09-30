4f85c2149f1665dd773151b6d3f8ee18a9758571
Romain Vaur <rvaur@hotmail.com>
2018-09-30 19:26:14 +0200

    first steps on writing content, welcome page with sidepanel

---
+++ front/src/Styles.css.js
@@ -0,0 +1,28 @@
+export default {
+  page: {
+    backgroundImage: 'url("images/bg1.jpg")',
+    backgroundSize: "cover",
+    height: "103vh"
+  },
+  title: {
+    fontFamily: "'Oswald', sans-serif",
+    color: "white",
+    fontSize: "5rem",
+    marginTop: "10vh",
+    letterSpacing: ".5rem"
+  },
+  paragraph: {
+    color: "white"
+  },
+  progress: {
+    background: "rgba(255, 255, 255, 0.7)",
+    borderRadius: "0rem"
+  },
+  button: {
+    borderRadius: "0px",
+    marginTop: "5vh"
+  },
+  networks: {
+    marginTop: "5vh"
+  }
+};

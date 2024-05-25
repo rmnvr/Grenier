96c39c8d4b08b45ad603633958f5305f80f597a0
Romain Vaur <rvaur@hotmail.com>
2024-05-25 21:41:30 +0200

    build

---
+++ front/build/precache-manifest.afcf544443ddb8b02115bbc53e03383e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80be8c2d39d1021fe28cc3caf52837f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb292505de991f12f27f",
+    "url": "/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "fb292505de991f12f27f",
+    "url": "/static/js/2.ab91c44c.chunk.js"
+  },
+  {
+    "revision": "70a4ce58a7453707dd1ec14bff006a90",
+    "url": "/static/js/2.ab91c44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073fee493cba66f3a49f",
+    "url": "/static/js/main.de06059a.chunk.js"
+  },
+  {
+    "revision": "2a204e0149ec9294cd5c",
+    "url": "/static/js/runtime-main.26551254.js"
+  }
+]);
